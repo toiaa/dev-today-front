f5267ad93bd7dc7d56439269824283001da8232a
toiaa <victoria@geddes.com.ar>
2024-06-25 11:35:40 +0200

    post cards fix

--- components/shared/cards/MeetupCard.tsx
+++ components/shared/cards/MeetupCard.tsx
@@ -23,7 +23,7 @@ const MeetUpCard = ({ post }: StandardCardProps) => {
             style={{ borderRadius: "10px", width: "72px", height: "72px" }}
           />
 
-          <div className="flex w-full overflow-hidden lg:gap-5">
+          <div className="flex w-full justify-between overflow-hidden lg:gap-5">
             <Link href={`/details/${post.id}`}>
               <p className="paragraph-3-bold lg:paragraph-1-bold h-[40px] w-full truncate text-dark-800 dark:text-white-100 lg:h-[22px]">
                 {title}
